cdb0f7e76aefc42f3b6c3f8d2c241714e760dd32
Dante004 <bartek.bk@tlen.pl>
2017-03-07 23:22:00 +0100

    Pierwsze skrypty

---
+++ Assets/Script/ChangeMaterial.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour {
+
+    public Material mat;
+
+
+    Renderer ren;
+
+    private void Start()
+    {
+        ren = GetComponent<Renderer>();
+        ren.enabled = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        sif (other.tag == "Player") other.material = ren.material;
+    }
+
+
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 5;
+    public float maxSpeed = 10;
+    public float minSpeed = 3;
+
+    private Rigidbody rb; 
+	
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1")) Run();
+        if (Input.GetButton("Fire2")) Slow();
+        if (Input.GetButtonUp("Fire1") || Input.GetButtonUp("Fire2")) speed = 5;
+
+
+    }
+
+
+    void FixedUpdate ()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(h, 0, v);
+        rb.velocity = movement * speed;	
+	}
+
+    void Run()
+    {
+        speed = maxSpeed;
+    }
+
+    void Slow()
+    {
+        speed = minSpeed;
+    }
+}
