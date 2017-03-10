8cbbd8d1f58c9497deee0e0d3e8db3ba9b514424
Dante004 <bartek.bk@tlen.pl>
2017-03-10 13:25:41 +0100

    Podarzanie kamery za graczem

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private Vector3 velocity;
+    public float smoothTimeX, smoothTimeZ;
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x,player.transform.position.x,ref velocity.x,smoothTimeX);
+        float posZ = Mathf.SmoothDamp(transform.position.z, player.transform.position.z, ref velocity.z, smoothTimeZ);
+
+        transform.position = new Vector3(posX, transform.position.y, posZ);
+    }
+}
