4874fffe486f4924e9037d95091f3b249544f56a
Dante004 <bartek.bk@tlen.pl>
2017-03-11 22:43:57 +0100

    Utworzenie sceny dla dema

---
+++ Assets/Script/Eney/EnemyMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    Vector3 m_from = new Vector3(0.0F, -30.0F, 0.0F);
+    Vector3 m_to = new Vector3(0.0F, -150.0F, 0.0F);
+    float m_frequency = 0.5F;
+
+    void Update ()
+    { 
+        Quaternion from = Quaternion.Euler(m_from);
+        Quaternion to = Quaternion.Euler(m_to);
+        float lerp = 0.5F * (1.0F + Mathf.Sin(Mathf.PI * Time.realtimeSinceStartup * this.m_frequency));
+
+        transform.localRotation = Quaternion.Lerp(from, to, lerp);
+    }
+}
