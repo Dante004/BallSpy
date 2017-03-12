9828caa448303eb667d6af8287ab9f91ae1c513f
Dante004 <bartek.bk@tlen.pl>
2017-03-12 17:44:17 +0100

    Dodanie możliwosci rozpoznawania materialu

---
+++ Assets/Script/Enemy/CheckCostume.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCostume : MonoBehaviour {
+
+    Renderer ren;
+    public Renderer player;
+
+    void Start()
+    {
+        ren = GetComponent<Renderer>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Renderer>();
+    }
+
+    public void m_CheckCostume()
+    {
+        if(ren.sharedMaterial == player.sharedMaterial)
+        {
+            return;
+        }
+        else
+        {
+            Debug.Log("Przeciwnik znalazł gracza");
+        }
+    }
+
+}
--- Assets/Script/Enemy/FieldofView.cs
+++ Assets/Script/Enemy/FieldofView.cs
@@ -22,8 +22,11 @@ public class FieldofView : MonoBehaviour
     public MeshFilter viewMeshFilter;
     Mesh viewMesh;
 
+    CheckCostume cc;
+
     void Start()
     {
+        cc = GetComponent<CheckCostume>();
         viewMesh = new Mesh();
         viewMesh.name = "View Mesh";
         viewMeshFilter.mesh = viewMesh;
@@ -54,14 +57,14 @@ public class FieldofView : MonoBehaviour
         for (int i = 0; i < targetsInViewRadius.Length; i++)
         {
             Transform target = targetsInViewRadius[i].transform;
-            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            Vector3 dirToTarget = (target.transform.position - transform.position).normalized;
             if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
             {
-                float dstToTarget = Vector3.Distance(transform.position, target.position);
+                float dstToTarget = Vector3.Distance(transform.position, target.transform.position);
                 if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
                 {
-                    visibleTargets.Add(target);
-                    Debug.Log("Przeciwnik widzi gracza");
+                    visibleTargets.Add(target.transform);
+                    cc.m_CheckCostume();
                 }
             }
         }
